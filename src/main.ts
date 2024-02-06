86118929fdc413ed6c5c6c2ee9d6fa01a5a26eb9
backend-kyounglyool <kr.park@fanmaum.com>
2024-02-07 02:35:38 +0900

    fix. main

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Temburine')
-    .setDescription('템버린 API')
-    .setVersion('1.0')
-    .addTag('default')
+    .setTitle("Temburine")
+    .setDescription("템버린 API")
+    .setVersion("1.0")
+    .addTag("default")
     .addBearerAuth(
       {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'Token',
-        in: 'header',
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "Token",
+        in: "header",
       },
-      'auth-user',
+      "auth-user"
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup("api-docs", app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
