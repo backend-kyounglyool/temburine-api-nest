9104832dd69491593eeb1f3be931eeb73f56cc17
backend-kyounglyool <kr.park@fanmaum.com>
2024-02-21 00:01:41 +0900

    fix. content-service

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ItemController } from "./item.controller";
 import { ItemService } from "./item.service";
 import { PrismaModule } from "../prisma/prisma.module";
+import { ContentModule } from "../content/content.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ContentModule],
   controllers: [ItemController],
   providers: [ItemService],
 })
